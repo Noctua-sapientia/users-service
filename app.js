a7ad428e7ef094a1185a301a136218b2b16e8d26
PabloSantosAlises <72519185+PabloSantosAlises@users.noreply.github.com>
2024-01-23 19:08:24 +0100

    Añadir comunicación con el microservicio de orders.
    Añadir comunicación con el microservicio de orders.

--- app.js
+++ app.js
@@ -24,5 +24,29 @@ app.use('/api/v1/sellers', sellersRouter);
 app.use('/api/v1/customers', customersRouter);
 app.use('/api/v1/login', loginRouter);
 
+// Swagger docuemntation setup
+var swaggerJsDoc = require('swagger-jsdoc');
+var swaggerUi = require('swagger-ui-express');
+var options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {title: 'Users API', version: '1.0.0'},
+        tags: [
+            {name: 'Users', description: 'Operations related to users.'}
+        ],
+    },
+    apis: ['./routes/sellers.js', './routes/customers.js'],
+};
+var swaggerSpec = swaggerJsDoc(options);
+
+// Routes
+app.use('/', indexRouter);
+app.use('/api/v1/sellers', sellersRouter);
+app.use('/api/v1/customers', customersRouter);
+app.use('/api/v1/login', loginRouter);
+app.use('/api/v1/apidocs/users', 
+    swaggerUi.serve, 
+    swaggerUi.setup(swaggerSpec));
+
 
 module.exports = app;
